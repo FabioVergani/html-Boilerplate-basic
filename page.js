c57a86ef1332b4b8af4823ee0d83024f313519ce
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-04-02 13:54:48 +0200

    Add files via upload

--- page.js
+++ page.js
@@ -1,16 +1,14 @@
-(function($){
-	function at(a,b,c){
-		function f(){a.removeEventListener(b,c)}
+($=>{
+	const at=(a,b,c)=>{
+		const f=()=>{a.removeEventListener(b,c)};
 		f();
 		a.addEventListener(b,c);
 		return f;
-	}
-	let f=at($,'DOMContentLoaded',function(x){
+	};
+	let f=at($,'DOMContentLoaded',x=>{
 		f();
 		f=null;
 		const $d=x.target;
 		console.dir($d);
-		//console.dir(getEventListeners(window));
 	});
 })(window);
-
